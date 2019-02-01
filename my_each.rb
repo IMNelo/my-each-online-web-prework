70995132f600a7b0e5dbc82bc66dfcb033315ac7
Agnelo Ribeiro <agnelo.ribeiro@gmail.com>
2019-02-01 00:48:09 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,13 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+	i = 0
+	while i < array.lenght
+	yield array[i]
+		i = i + 1
+	end
+end
+
+my_each(['hi', 'hello', 'bye', 'goodbye']) do |name|
+  if name.start_with?("T")
+    puts "Hi, #{name}"
+  end
 end
